4216a6a305f62a8fd0f6079285e1ee47e9d384ba
TOM <txm.money@gmail.com>
2019-08-22 09:05:28 +0900

    Initial commit

--- src/controllers/indexServlet.java
+++ src/controllers/indexServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
 import utils.DBUtil;
 
 
@@ -41,17 +41,17 @@ public class indexServlet extends HttpServlet {
         try {
             page = Integer.parseInt(request.getParameter("page"));
         } catch(NumberFormatException e) {}
-        
+
         // 最大件数と開始位置を指定してメッセージを取得
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+        List<Task> messages = em.createNamedQuery("getAllMessages", Task.class)
                                    .setFirstResult(15 * (page - 1))
                                    .setMaxResults(15)
                                    .getResultList();
-        
+
         // 全件数を取得
         long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class)
                                       .getSingleResult();
-        
+
         em.close();
 
         request.setAttribute("messages", messages);
@@ -65,7 +65,7 @@ public class indexServlet extends HttpServlet {
             request.getSession().removeAttribute("flush");
         }
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
     }
 }
